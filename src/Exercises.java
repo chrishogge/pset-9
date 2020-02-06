842481e1fc912f285d2824f96c8e5abce8a567bd
Admin <chogge@ucvts.org>
2020-02-05 22:10:48 -0500

    Exercise 6 Started

--- src/Exercises.java
+++ src/Exercises.java
@@ -130,7 +130,17 @@ public class Exercises {
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
-		// write your code here
+		if (numbers == null || numbers.length < 3) {
+			return false;
+		} else if(!(numbers == null || numbers.length < 3)) {
+			for(int i = 1; i < numbers.length-1; i++) {
+				if(numbers[i] > numbers[i-1] && numbers[i] < numbers[i+1]) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
 		
 		return false;	// default return value to ensure compilation
 	}
